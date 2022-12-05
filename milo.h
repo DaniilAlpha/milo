3eb49ca778ea48a30add6927c63e47c8330bf8ee
Daniil Alpha <59438356+DaniilAlpha@users.noreply.github.com>
2022-12-05 18:57:28 +0300

    Create milo.h

---
+++ milo.h
@@ -0,0 +1,36 @@
+#define MILO_ALL 5
+#define MILO_INFO 4
+#define MILO_WARNING 3
+#define MILO_ERROR 2
+#define MILO_FATAL 1
+#define MILO_SILENT 0
+
+#if MILO_LVL >= MILO_ALL
+#define deb(fmt, ...) printf("[deb] " fmt "\n", ##__VA_ARGS__)
+#else
+#define deb(fmt, ...)
+#endif
+
+#if MILO_LVL >= MILO_INFO
+#define inf(fmt, ...) printf("[inf] " fmt "\n", ##__VA_ARGS__)
+#else
+#define inf(fmt, ...)
+#endif
+
+#if MILO_LVL >= MILO_WARNING
+#define war(fmt, ...) printf("[war] " fmt "\n", ##__VA_ARGS__)
+#else
+#define war(fmt, ...)
+#endif
+
+#if MILO_LVL >= MILO_ERROR
+#define err(fmt, ...) printf("[err] " fmt "\n", ##__VA_ARGS__)
+#else
+#define err(fmt, ...)
+#endif
+
+#if MILO_LVL >= MILO_FATAL
+#define fatal(fmt, ...) printf("[fatal] " fmt "\n", ##__VA_ARGS__)
+#else
+#define fatal(fmt, ...)
+#endif
