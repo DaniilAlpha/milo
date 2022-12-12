f75002c701e500239f8d5becf295ad9ba16c830f
Daniil Alpha <59438356+DaniilAlpha@users.noreply.github.com>
2022-12-12 22:26:55 +0300

    Trace color changet to cyan

--- include/milo_text_attr.h
+++ include/milo_text_attr.h
@@ -6,7 +6,7 @@
 #endif
 
 #if MILO_USE_TA
-#define MILO_TA_TRACE "\x1B[0;35m"
+#define MILO_TA_TRACE "\x1B[0;36m"
 #define MILO_TA_INFO "\x1B[0;32m"
 #define MILO_TA_WARN "\x1B[0;33m"
 #define MILO_TA_ERROR "\x1B[0;31m"
