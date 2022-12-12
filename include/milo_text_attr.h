07f8a525227b3b2af5f2463fdb5f5103c15e1e1f
Daniil Alpha <59438356+DaniilAlpha@users.noreply.github.com>
2022-12-12 22:04:26 +0300

    Fatals are now different from errors

--- include/milo_text_attr.h
+++ include/milo_text_attr.h
@@ -6,17 +6,19 @@
 #endif
 
 #if MILO_USE_TA
-#define MILO_TA_BLUE "\x1B[0;35m"
-#define MILO_TA_GREEN "\x1B[0;32m"
-#define MILO_TA_YELLOW "\x1B[0;33m"
-#define MILO_TA_RED "\x1B[0;31m"
+#define MILO_TA_TRACE "\x1B[0;35m"
+#define MILO_TA_INFO "\x1B[0;32m"
+#define MILO_TA_WARN "\x1B[0;33m"
+#define MILO_TA_ERROR "\x1B[0;31m"
+#define MILO_TA_FATAL "\x1B[1;31m"
 #define MILO_TA_CLEAR "\x1B[0m"
 #else
-#define MILO_TA_BLUE
-#define MILO_TA_GREEN
-#define MILO_TA_YELLOW
-#define MILO_TA_RED
-#define MILO_TA_CLEAR
+#define MILO_TA_TRACE 
+#define MILO_TA_INFO 
+#define MILO_TA_WARN 
+#define MILO_TA_ERROR 
+#define MILO_TA_FATAL 
+#define MILO_TA_CLEAR 
 #endif
 
 #endif
